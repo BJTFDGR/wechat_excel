47d2740e86c592459899aeb94e6c5cd62c816ef4
BJTFDGR <chenbc@sjtu.edu.cn>
2018-09-14 19:55:18 +0800

    Initial commit

---
+++ app/src/main/java/wechat/wechat/wechat_excel/wechatdatastore.java
@@ -0,0 +1,18 @@
+package wechat.wechat.wechat_excel;
+
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+
+public class wechatdatastore {
+
+    String nickname;
+    String username;
+    String alias;
+
+    public wechatdatastore(String nickname, String username, String alias) {
+        this.nickname = nickname;
+        this.username = username;
+        this.alias = alias;
+    }
+}
